7c830195c137dcbd466c0eb9cb117caa2b81a013
Lucas Silva <lucas.silva_1994@outlook.com>
2022-09-17 22:47:33 -0300

    Alteração na visibilidade de atributos e implementando métodos da classe responsável por retornar valores dos atributos

--- js/app/models/Negociacao.js
+++ js/app/models/Negociacao.js
@@ -1,13 +1,25 @@
 class Negociacao{
     //Iniciando a classe com propriedade e já atribuindo valores.
     constructor(data, quantidade, valor){
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
 
     //Método responsável por retonar o volume.
-    obterVolume(){
-        return this.quantidade * this.valor;
+    getVolume(){
+        return this._quantidade * this._valor;
+    }
+
+    getData(){
+        return this._data;
+    }
+
+    getQuantidade(){
+        return this._quantidade;
+    }
+     
+    getValor(){
+        return this._valor;
     }
 }
