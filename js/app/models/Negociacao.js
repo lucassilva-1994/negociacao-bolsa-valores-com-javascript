efb64f6ca266fdaa7a7b6801b5a9caff8c05eab7
Lucas Silva <lucas.silva_1994@outlook.com>
2022-09-17 21:01:29 -0300

    Criando a classe Negociação e criando um objeto dessa classe no index.html

---
+++ js/app/models/Negociacao.js
@@ -0,0 +1,8 @@
+class Negociacao{
+    //Iniciando a classe com propriedade e já atribuindo valores.
+    constructor(){
+        this.data = new Date();
+        this.quantidade = 0;
+        this.valor = 0.0;
+    }
+}
