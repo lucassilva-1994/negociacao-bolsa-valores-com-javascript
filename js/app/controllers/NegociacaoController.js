36762d013ef9a5b7d255488e79700eb23c9eefa5
Lucas Silva <lucas.silva_1994@outlook.com>
2022-09-18 12:29:45 -0300

    Atribuindo o aliás para document.querySelector

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -1,6 +1,14 @@
 class NegociacaoController{
     adiciona(event){
         event.preventDefault();
-        alert("Oi");
+        //Atribuindo um aliás para o document.querySelector.
+        let $ = document.querySelector.bind(document);
+        let inputData = $("#data");
+        let inputQuantidade = $("#quantidade");
+        let inputValor = $("#valor");
+
+        console.log(inputData.value);
+        console.log(inputQuantidade.value);
+        console.log(inputValor.value);
     }
 }
