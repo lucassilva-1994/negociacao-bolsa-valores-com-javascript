9d3b073867ad83027a96b4e8c313f2ab9e7864f3
Lucas Silva <lucas.silva_1994@outlook.com>
2022-09-20 22:51:57 -0300

    Criando classe responsável por guardar e  criar negociações, e implementações dos métodos privados limpar formulário e adicionar negociações no NegociacaoController

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -4,6 +4,7 @@ class NegociacaoController{
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event){
@@ -11,14 +12,27 @@ class NegociacaoController{
         //Chamando o método estático textoParaData
         let data = DateHelper.textoParaData(this._inputData.value);
 
-        let negociacao = new Negociacao(
-                data,
-                this._inputQuantidade.value,
-                this._inputValor.value
-            );
-        console.log(negociacao);
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+        console.log(this._listaNegociacoes.negociacoes);
+    }
 
-        //Chamando o método estático dataParatexto
-        console.log(DateHelper.dataParaTexto(negociacao.data));
+    //Método responsável por limpar o formulário após a negociação ser concluída.
+    _limpaFormulario(){
+        //Atribuindo valores padrões
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        //Após a negociação o campo data receberá o focus.
+        this._inputData.focus();
+    }
+
+    //Método auxiliar para criar uma nova negociação.
+    _criaNegociacao(){
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
     }
 }
---
+++ js/app/models/LIstaNegociacoes.js
@@ -0,0 +1,17 @@
+class ListaNegociacoes{
+    constructor(){
+        //Criando uma lista de negociações
+        this._negociacoes = [];
+    }
+
+    adiciona(negociacao){
+        //Adicionando uma negociação na lista de negociações.
+        this._negociacoes.push(negociacao);
+    }
+
+    //Criando método para retornar as negociações
+
+    get negociacoes(){
+        return this._negociacoes;
+    }
+}
