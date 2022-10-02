2f327797285f468da5ccfae4f90ec3c64bc68d6f
Lucas Silva <lucas.silva_1994@outlook.com>
2022-10-01 21:59:24 -0300

    Ordenando tabelas ao clicar em colunas, implementando o método post em HttpService e criando uma página que contém o formulário de enviar negociações

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -4,11 +4,12 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._ordemAtual = '';
 
         this._listaNegociacoes = new Bind (
                 new ListaNegociacoes(),
                 new NegociacoesView($('#negociacoesView')),
-                'adiciona', 'esvazia');
+                'adiciona', 'esvazia','ordena','inverteOrdem');
         this._mensagem = new Bind(
             new Mensagem(),
             new MensagemView($('#mensagemView')),
@@ -20,6 +21,16 @@ class NegociacaoController{
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._mensagem.texto = 'Negociacao adicionada com sucesso.';
         this._limpaFormulario();
+    }
+
+    //Método responsável por ordenar a tabela de acordo com a coluna.
+    ordena(coluna) {
+        if(this._ordemAtual == coluna) {
+            this._listaNegociacoes.inverteOrdem();
+        } else {
+            this._listaNegociacoes.ordena((a, b) => a[coluna] - b[coluna]);    
+        }
+        this._ordemAtual = coluna;
     }
 
     importaNegociacoes() {
